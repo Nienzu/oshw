60836597fd35056f81d8ce90fad1969c3ddcbd0a
Tommy <MacWu@TommydeMacBook-Pro.local>
2015-10-22 11:21:06 +0800

    Add the comment of each function.

--- client.c
+++ client.c
@@ -5,46 +5,61 @@
 #include<sys/types.h>
 #include<netinet/in.h>
 
-#define B_SIZE 256  //Buffer_size
-#define F_N_SIZE 256 //File_Name_size
+#define B_SIZE 1024  //Buffer_size
+#define F_N_SIZE 1024 //File_Name_size
 
 void edit_file(int socket_fd, char filename[F_N_SIZE]);
 int check_file(int socket, char filename[F_N_SIZE]);
-int send_adv(int socket_fd, char buffer[B_SIZE]);
+void send_adv(int socket_fd, char buffer[B_SIZE]);
 void create_file(int socekt_fd, char filename[F_N_SIZE]);
 void list_file(int socket_fd);
 void download_file(int socket_fd, char filename[F_N_SIZE]);
+void clear_buffer();
 
 int main(){
 	int socket_fd;
 	struct sockaddr_in dest;
 	char buffer[B_SIZE];
 	socket_fd = socket(PF_INET, SOCK_STREAM, 0);
-	
+	char ip[20];
+	bzero(&ip, 20);
+	char command='\0';
+	printf("Please enter the adress you want to connect:");
+	scanf("%s", ip);
+	scanf("%c", &command); // clear the \n 
+
 	/*Initialize dest*/
 	bzero(&dest, sizeof(struct sockaddr_in));
 	dest.sin_family = PF_INET;
 	dest.sin_port = htons(8889);
-	inet_aton("127.0.0.1", &dest.sin_addr);
+	inet_aton(ip, &dest.sin_addr);
 	
 	/*connecting to server*/
 	int d = connect(socket_fd, (struct sockaddr* )&dest, sizeof(dest));
-	char command='\0';
-	char filename[128];
+	char filename[F_N_SIZE];
+
+	/* This is the main function of client. */
 	while(1){
 		bzero(&buffer, B_SIZE);
 		bzero(&filename, F_N_SIZE);
-		printf("==================================\nThis is command list:\n(C)在clinet端輸入檔名，在server端建立一空檔案\n(E)對server端上存在的檔案進行編輯並儲存\n(R)刪除server端的檔案\n(L)顯示server端上所有的檔案\n(D)輸入欲下載的檔名，從server端下載檔案\n(Q)離開。\n===========================\n");
+		printf("==================================\nThis is command list:\n(C)在clinet端輸入檔名，在server端建立一空檔案\n(E)對server端上存在的檔案進行編輯並儲存\n(R)刪除server端的檔案\n(L)顯示server端上所有的檔案\n(D)輸入欲下載的檔名，從server端下載檔案\n(Q)離開。\n==================================\n");
 		printf("Please enter command:");
 		scanf("%c", &command);
+		command = tolower(command);
 		send(socket_fd, &command, sizeof(command), 0);
 		switch(command){
 			case 'c' :
 				printf("Enter the Filename:");
 				scanf("%s", filename);
-				scanf("%c", &command);//clear the std input buffer
-				create_file(socket_fd, filename);
-				break;
+				scanf("%c", &command);//clear \n
+				if(check_file(socket_fd, filename == 0){
+					create_file(socket_fd, filename);
+					break;
+				}
+				else{
+					printf("File is exist.\n");
+					break;
+				}
 			case 'e':
 				printf("Enter the file name:");
 				scanf("%s", filename);
@@ -76,7 +91,10 @@ int main(){
 				break;
 			case 'q':
 				break;
+			default:
+				printf("Oops, here is no such command!!\n");
 
+				break;
 		}
 		if(command == 'q')
 			break;
@@ -85,21 +103,27 @@ int main(){
 	return 0;
 }
 
+/* edit the file on server 
+Variable
+	check : presnet the return value of check_file
+	buffer : use to store the cotent to send.
+	clear : use to clear \n
+*/
 void edit_file(int socket_fd, char filename[F_N_SIZE]){
 	int check = check_file(socket_fd, filename);		
 	char buffer[B_SIZE];
 	char clear;
 	if(check == 1){
-		printf("You can now edit %s now.\n", filename);
+		printf("You can edit %s now.\n", filename);
 		printf("When you see $ mean you can edit, if not means the content is being transfer.\n");
 		printf("You can enter : \"!q\" to finish the edit.\n");
 		while(1){
 			bzero(&buffer, B_SIZE);
 			printf("$:");
-			scanf("%[^\n]", buffer);
+			scanf("%[^\n]", buffer); //let scanf to accept space as input
 			scanf("%c", &clear);
 			write(socket_fd, buffer, B_SIZE);	
-			if(strcmp(buffer, "!q")==0)
+			if(strcmp(buffer, "!q")==0) //if user type only !q, will close the edit and save the file.
 				break;
 		}
 	}
@@ -108,20 +132,26 @@ void edit_file(int socket_fd, char filename[F_N_SIZE]){
 	
 }
 
+/* download the file from server to client
+Variable
+	check : presnet the return value of check_file
+	brecv : record the real byte recived from server
+	buffer : use to store temp content to write.
+ */
 void download_file(int socket_fd, char filename[F_N_SIZE]){	
 	int check = check_file(socket_fd, filename);
 	if(check==1){
 		int brecv;
 		char buffer[B_SIZE];
-		bzero(&buffer, B_SIZE);
-		FILE *fp = fopen(filename, "ab");
+		bzero(&buffer, B_SIZE);  // Always set the buffer to empty
+		FILE *fp = fopen(filename, "ab");   // download by bit
 		while(check == 1){
 		brecv = read(socket_fd, buffer, B_SIZE);
-		printf("Byte recv : %d\n", brecv);
-		fwrite(buffer, 1, brecv, fp);
+		printf("Byte recv : %d\n", brecv); //print the download process.
+		fwrite(buffer, 1, brecv, fp); 
 		if(brecv < B_SIZE)
 			break;
-
+		/* when  the read bit is less than B_SIZE, meaning finishing the download. */
 		}
 		fclose(fp);
 	}
@@ -130,6 +160,10 @@ void download_file(int socket_fd, char filename[F_N_SIZE]){
 	
 }
 
+/*  list the file on server. 
+Variable 
+	buffer : use to store temporary content to write.
+*/
 void list_file(int socket_fd){
 	char buffer[B_SIZE];
 	bzero(&buffer, B_SIZE);
@@ -137,16 +171,25 @@ void list_file(int socket_fd){
 		recv(socket_fd, buffer, B_SIZE, 0);
 		printf("%s", buffer);
 		if(buffer[0]=='\0')
-			break;
+			break;   // receive until the  server's popen's return value 's end.
 	}
 
 }
 
+/* Create command. */
 void create_file(int socket_fd , char filename[F_N_SIZE]){
-	send_adv(socket_fd, filename);	
+	send_adv(socket_fd, filename);
+	/* just send the filename created. */
 }
 
+/* This is normally use in many function to check the file exist in server or not. 
+Variable 
+	check : present the value of check_file
 
+Return value
+	1 : file exist
+	-1 : file does not exist
+*/
 int check_file(int socket_fd, char filename[F_N_SIZE]){
 	char check; 
 	send_adv(socket_fd, filename);
@@ -155,9 +198,8 @@ int check_file(int socket_fd, char filename[F_N_SIZE]){
 	return check == 'y' ? 1 : -1 ;
 }
 
-/*Rewrite send function with check*/
-int send_adv(int socket_fd, char buffer[B_SIZE]){
-	int check=0;
-	check = send(socket_fd, buffer, B_SIZE, 0);	
-	return check;
+/*Rewrite send function with check.Just for convinient...*/
+void send_adv(int socket_fd, char buffer[B_SIZE]){
+	send(socket_fd, buffer, B_SIZE, 0);	
 }
+
--- server.c
+++ server.c
@@ -5,8 +5,8 @@
 #include<netinet/in.h>
 #include<sys/types.h>
 
-#define B_SIZE 256
-#define F_N_SIZE 256
+#define B_SIZE 1024   //Buffer max size
+#define F_N_SIZE 1024 // Filename max size
 
 
 void list_file(int client);
@@ -40,18 +40,23 @@ int main(){
 	int check=0;
 	char command='\0';
 	while(1){
-		bzero(&buffer, B_SIZE);
+		bzero(&buffer, B_SIZE);  /*always clear the buffer*/
 		recv(client, &command, sizeof(command), 0);
 		switch(command){
 		case 'c':
 			recv(client, &buffer, B_SIZE, 0);
-			fp = fopen(buffer, "w");
-			fclose(fp);
-			break;
+			check = check_file(client, buffer);
+			//only when file doesn't exist, server will create the file.
+			if(check == -1){
+				fp = fopen(buffer, "w");
+				fclose(fp);
+				break;
+			}
 		case 'e':
 			edit_file(client);
 			break;
 		case 'r':
+			/* Here is the remove function. */
 			recv(client, &buffer, B_SIZE, 0);
 			check = check_file(client, buffer);
 			if(check==1)
@@ -73,20 +78,26 @@ int main(){
 	return 0;
 }
 
+/* edit the existing file on server. 
+Variable 
+	buffer : use to store the cotent to send.
+	filename : file's name
+	check : use to present the return value of check_file
+*/
 void edit_file(int client){
 	char buffer[B_SIZE];
 	char filename[F_N_SIZE];
 	bzero(&buffer, B_SIZE);
-	bzero(&filename, F_N_SIZE);
+	bzero(&filename, F_N_SIZE); //always clear ...
 	recv(client, &filename, F_N_SIZE, 0);
 	int check = check_file(client, filename);
-	int brecv=0;
+	//first must check the file is exsting or not.
 	if(check == 1){
-		FILE *fp = fopen(filename, "a");
+		FILE *fp = fopen(filename, "a"); // edit file but not clean the original content.
 		while(1){
 			bzero(&buffer, B_SIZE);
 			read(client, buffer, B_SIZE);
-			if(buffer[0]=='!' && buffer[1]=='q')
+			if(buffer[0]=='!' && buffer[1]=='q') //when client send the !q will close the file and save.(just like vim...)
 				break;
 			fprintf(fp, "%s\n", buffer);
 		}
@@ -94,47 +105,72 @@ void edit_file(int client){
 	}
 }
 
+/* just get the current file on server and send the list to client. 
+Variable 
+	cccc : is to present the command I may use.But here is just to represent ls.
+	buffer : use to store the cotent to send.
+*/
 void list_file(int client){
 	char cccc[100];
 	bzero(&cccc, 100);
 	cccc[0]='l';
 	cccc[1]='s';
+	//just use popen to implement the "ls" command and send it to client.
 	FILE *fp = popen(cccc, "r");
 	char buffer[B_SIZE];
 	bzero(&buffer, B_SIZE);
 	while((fgets(buffer, B_SIZE, fp) != NULL)){	
 		send(client, buffer, B_SIZE, 0);
-		bzero(&buffer, B_SIZE);
-	}
+		bzero(&buffer, B_SIZE); //always clear the buffer
+	}//gets the output of ls and send filename one by one to client.
 	send(client, buffer, B_SIZE, 0);	
 }
 
+
+/* corresponding the download function in client. 
+Variable
+	filename : the file to transfer
+	check : use to present the return value of check_file
+	fp : file pointer
+	buffer : use to send bit.
+	bread : record the real byte read from the file.
+*/
 void trans_file(int client){
 	char filename[F_N_SIZE];
 	bzero(&filename, F_N_SIZE);
 	recv(client, &filename, F_N_SIZE, 0);
-	int i = check_file(client, filename);
+	int check = check_file(client, filename);
 	FILE *fp = fopen(filename, "rb");
+	//read the file by byte, then send it by byte.
 	char buffer[B_SIZE];
-	int bread ;
-	while(i == 1){
+	int bread;
+	while(check == 1){
 		bzero(&buffer, B_SIZE);
 		bread = fread(buffer, 1, B_SIZE, fp);
+		//record the actually read in bit to control  the  transfer process.
 		if(bread > 0){	
 			write(client, buffer, bread);	
-			printf("Byte read %d:\n", bread);
+			printf("Byte read %d:\n", bread);// just print the transfering process.
 		}
-		if(bread < B_SIZE){
+		if(bread < B_SIZE){ // when bread
 			if(feof(fp))
 				printf("End of file.Copy complete.\n");
 			if(ferror(fp))
-				printf("error reading\n");
+				printf("error reading\n"); 
 			break;
 		}
 	}
 }
+
+/* corresponding the check_file gunction to client.c 
+Variable
+	check : to present the file is exsting or not.
+
+Return value
+	1 : file exist 
+	-1 : file does not exist
+*/
 int check_file(int client, char filename[F_N_SIZE]){
-	char buffer[B_SIZE];
 	char check='\0';
 	FILE *fp = fopen(filename, "r");
 	if(fp == NULL){
