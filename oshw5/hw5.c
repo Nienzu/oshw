943462e69d1a8ee34522dedbc0a964f94ac98305
nienzu <ibqqz0602@gmail.com>
2016-01-14 15:29:18 +0800

    opt1

--- oshw5/hw5.c
+++ oshw5/hw5.c
@@ -4,12 +4,13 @@
 
 int size;
 char fs_name[100];
+
 int myfs_file_open(const char *filename){
-	FILE *fs;
-	fs = fopen(fs_name,"rb");
+	FILE *File_sys;
+	File_sys = fopen(fs_name,"rb");
 	char block[1024];
 	bzero(block,1024);
-	fread(block,sizeof(char),1024,fs);
+	fread(block,sizeof(char),1024,File_sys);
 	char *check;
 	check = strstr(block,filename);
 	if(check == NULL){
@@ -36,13 +37,14 @@ int myfs_file_close(int fd){
 }
 
 int myfs_file_create(const char *filename){
-	FILE *fs,*ftmp;
-	fs = fopen(fs_name,"rb");
-	ftmp = fopen("fs_tmp","wb");
+	FILE *File_sys
+	FILE *duplicate;
+	File_sys = fopen(fs_name,"rb");
+	duplicate = fopen("fs_tmp","wb");
 	char block[1024],tmp[1024];
 	bzero(block,1024);
 	bzero(tmp,1024);
-	fread(block,sizeof(char),1024,fs);
+	fread(block,sizeof(char),1024,File_sys);
 	char *check;
 	check = strstr(block,filename);
 	if(check != NULL){
@@ -51,43 +53,43 @@ int myfs_file_create(const char *filename){
 	}
 	int i,location = 0;
 	for(;block[0] != '\0';++location){
-		fread(block,sizeof(char),1024,fs);
+		fread(block,sizeof(char),1024,File_sys);
 		if(location == size){
 			printf("system is full!\n");
 			return 0;
 		}
 	}
-	rewind(fs);
-	fread(block,sizeof(char),1024,fs);
+	rewind(File_sys);
+	fread(block,sizeof(char),1024,File_sys);
 	strcat(block,filename);
 	sprintf(tmp," %d",location);
 	strcat(block,tmp);
 	strcat(block," ");
 	for(i = 0;i < size;++i){
 		if(i == location){
-			fwrite(filename,sizeof(char),30,ftmp);
+			fwrite(filename,sizeof(char),30,duplicate);
 			bzero(tmp,1024);
-			fwrite(tmp,sizeof(char),994,ftmp);
+			fwrite(tmp,sizeof(char),994,duplicate);
 		}
 		else
-			fwrite(block,sizeof(char),1024,ftmp);
+			fwrite(block,sizeof(char),1024,duplicate);
 		if(i != size -1)
-			fread(block,sizeof(char),1024,fs);
+			fread(block,sizeof(char),1024,File_sys);
 	}
-	fclose(fs);
-	fclose(ftmp);
+	fclose(File_sys);
+	fclose(duplicate);
 	rename("fs_tmp",fs_name);
 }
 
 int myfs_file_delete(const char *filename){
-	FILE *fs,*ftmp;
-	fs = fopen(fs_name,"rb");
-	ftmp = fopen("fs_tmp","wb");
+	FILE *File_sys,*duplicate;
+	File_sys = fopen(fs_name,"rb");
+	duplicate = fopen("fs_tmp","wb");
 	char block[1024],tmp[1024],name[30];
 	bzero(block,1024);
 	bzero(tmp,1024);
 	bzero(name,30);
-	fread(block,sizeof(char),1024,fs);
+	fread(block,sizeof(char),1024,File_sys);
 	char *check;
 	check = strstr(block,filename);
 	if(check == NULL){
@@ -98,7 +100,7 @@ int myfs_file_delete(const char *filename){
 	sprintf(name,"%d ",size);
 	strcpy(tmp,name);
 	for(i = 1;i < size;++i){
-		fread(block,sizeof(char),1024,fs);
+		fread(block,sizeof(char),1024,File_sys);
 		strncpy(name,block,30);
 		if(strcmp(name,filename) != 0 && name[0] != '\0'){
 			strcat(tmp,name);
@@ -109,26 +111,26 @@ int myfs_file_delete(const char *filename){
 		}
 	}
 	bzero(name,30);
-	fseek(fs,1024,SEEK_SET);
+	fseek(File_sys,1024,SEEK_SET);
 	for(i = 0;i < size;++i){
 		if(strcmp(name,filename) == 0)
 			bzero(tmp,1024);
-		fwrite(tmp,sizeof(char),1024,ftmp);
+		fwrite(tmp,sizeof(char),1024,duplicate);
 		if(i != size - 1){
-			fread(tmp,sizeof(char),1024,fs);
+			fread(tmp,sizeof(char),1024,File_sys);
 			bzero(name,31);
 			strncpy(name,tmp,30);	
 		}
 	}
-	fclose(fs);
-	fclose(ftmp);
+	fclose(File_sys);
+	fclose(duplicate);
 	rename("fs_tmp",fs_name);
 }
 
 int myfs_file_read(int fd,char *buf,int count){
 	int i,max = 0,read = 0;
-	FILE *fs;
-	fs = fopen(fs_name,"rb");
+	FILE *File_sys;
+	File_sys = fopen(fs_name,"rb");
 	char block[1024],header[30],number[4];
 	bzero(block,1024);
 	bzero(header,30);
@@ -141,18 +143,18 @@ int myfs_file_read(int fd,char *buf,int count){
 		--count;
 	}
 	count = 0;
-	fseek(fs,fd * 1024,SEEK_SET);
-	fread(block,sizeof(char),1024,fs);
+	fseek(File_sys,fd * 1024,SEEK_SET);
+	fread(block,sizeof(char),1024,File_sys);
 	strncpy(header,block,30);
 	if(max != 0)
 		strncpy(buf,block + 34,990);
 	else
 		strncpy(buf,block + 34,read);
-	rewind(fs);
+	rewind(File_sys);
 	while(count != max){
 		++count;
 		for(i = 0;i < size;++i){
-			fread(block,sizeof(char),1024,fs);
+			fread(block,sizeof(char),1024,File_sys);
 			strncpy(number,block + 30,4);
 			if(strncmp(header,block,30) == 0 && count == atoi(number)) 
 				if(count == max)
@@ -160,9 +162,9 @@ int myfs_file_read(int fd,char *buf,int count){
 				else
 					strncat(buf,block + 34,990);
 		}
-		rewind(fs);
+		rewind(File_sys);
 	}
-	fclose(fs);
+	fclose(File_sys);
 	printf("******Content******\n");
 	printf("%s\n",buf);
 	printf("********END********\n");
@@ -170,16 +172,16 @@ int myfs_file_read(int fd,char *buf,int count){
 }
 
 int myfs_file_write(int fd,char *buf,int count){
-	FILE *fs,*ftmp;
-	fs = fopen(fs_name,"rb");
-	ftmp = fopen("fs_tmp","wb");
+	FILE *File_sys,*duplicate;
+	File_sys = fopen(fs_name,"rb");
+	duplicate = fopen("fs_tmp","wb");
 	char block[1024],header[30],number[4],tmp;
 	bzero(block,1024);
 	bzero(header,34);
 	bzero(number,4);
 	int i,space = 0,write;
 	for(i = 0;i < size;++i){
-		fread(block,sizeof(char),1024,fs);
+		fread(block,sizeof(char),1024,File_sys);
 		if(block[0] == '\0')
 			++space;
 	}
@@ -198,54 +200,54 @@ int myfs_file_write(int fd,char *buf,int count){
 		--space; 
 	}
 	count = 0;
-	rewind(fs);
+	rewind(File_sys);
 	for(i = 0;i < size;++i){
-		fread(block,sizeof(char),1024,fs);
+		fread(block,sizeof(char),1024,File_sys);
 		if(i != fd)
-			fwrite(block,sizeof(char),1024,ftmp);
+			fwrite(block,sizeof(char),1024,duplicate);
 		else{
 			strncpy(header,block,30);
-			fwrite(block,sizeof(char),34,ftmp);
-			fwrite(buf,sizeof(char),write,ftmp);
+			fwrite(block,sizeof(char),34,duplicate);
+			fwrite(buf,sizeof(char),write,duplicate);
 			bzero(block,1024);
-			fwrite(block,sizeof(char),990-write,ftmp);
+			fwrite(block,sizeof(char),990-write,duplicate);
 			--space;
 			++count;
 		}
 	}
-	fclose(fs);
-	fclose(ftmp);
+	fclose(File_sys);
+	fclose(duplicate);
 	rename("fs_tmp",fs_name);
 	while(space > -1){
-		fs = fopen(fs_name,"rb");
-		ftmp = fopen("fs_tmp","wb");
+		File_sys = fopen(fs_name,"rb");
+		duplicate = fopen("fs_tmp","wb");
 		bzero(block,1024);
 		sprintf(number,"%d",count);
 		for(i = 0;i < size;++i){
-			fread(block,sizeof(char),1024,fs);
+			fread(block,sizeof(char),1024,File_sys);
 			if(block[0] != '\0')
-				fwrite(block,sizeof(char),1024,ftmp);
+				fwrite(block,sizeof(char),1024,duplicate);
 			else{
 				bzero(block,1024);
 				strcpy(block,header);
 				strcat(block,number);
-				fwrite(block,sizeof(char),34,ftmp);
-				fwrite(buf+990*count,sizeof(char),write,ftmp);
+				fwrite(block,sizeof(char),34,duplicate);
+				fwrite(buf+990*count,sizeof(char),write,duplicate);
 				bzero(block,1024);
-				fwrite(block,sizeof(char),990-write,ftmp);
+				fwrite(block,sizeof(char),990-write,duplicate);
 				++count;
 				--space;
 			}
 		}
-		fclose(fs);
-		fclose(ftmp);
+		fclose(File_sys);
+		fclose(duplicate);
 		rename("fs_tmp",fs_name);
 	}
 	free(buf);
 }	
 
 int main(int argc,char **argv){
-	FILE *fs;
+	FILE *File_sys;
 	char ch,n,block[1024],tmp[1024],name[30];
 	char *buf;
 	int fd = 0,check = 1,count;
@@ -254,14 +256,15 @@ int main(int argc,char **argv){
 	bzero(name,30);
 	bzero(fs_name,100);
 	strcpy(fs_name,argv[1]);
-	fs = fopen(argv[1],"rb");
-	fread(block,sizeof(char),1024,fs);
-	printf("%s\n",block);
+	File_sys = fopen(argv[1],"rb");
+
+	fread(block,sizeof(char),1024,File_sys);
 	sscanf(block,"%d%s",&size,tmp);
-	printf("%d\n",size);
-	rewind(fs);
+	printf("Name of the File system: %s\n", fs_name);
+	printf("Size of %s : %d KB\n", fs_name, size);
+	rewind(File_sys);
 	int i;
-	fclose(fs);
+	fclose(File_sys);
 	while(check){
 		if(fd == 0){
 			printf("(O)pen the file\n");
