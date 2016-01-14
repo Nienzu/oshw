26cdd1155d003cdff61b06db80b40e2861547bc0
nienzu <ibqqz0602@gmail.com>
2016-01-14 15:17:39 +0800

    Change the style.

--- oshw5/Create_FS.c
+++ oshw5/Create_FS.c
@@ -3,50 +3,53 @@
 #include <string.h>
 
 int myfs_create(const char *filesystemname,int max_size){
-	FILE *fs;
-	fs = fopen(filesystemname,"wb");
+	FILE *File_sys;
+	File_sys = fopen(filesystemname,"wb");
 	int i;
 	char buf[1024];
-	char s[10];
-	bzero(s,10);
+	char size[10];
+	bzero(size,10);
 	bzero(buf,1024);
+
 	for(i = 0; i < max_size;++i)
-		fwrite(buf,sizeof(char),1024,fs);
-	rewind(fs);
-	sprintf(s,"%d",max_size);
-	strcat(s," ");
-	fwrite(s,sizeof(char),10,fs);
-	fclose(fs);
+		fwrite(buf,sizeof(char),1024,File_sys);
+	rewind(File_sys);
+	sprintf(size,"%d",max_size);
+	strcat(size," ");
+	fwrite(size,sizeof(char),10,File_sys);
+	fclose(File_sys);
+	return 0;
 }
 int myfs_destroy(const char *filesystemname){
 	remove(filesystemname);
+	return 0;
 }
 int main(){
 	int check = 1;
-	char ch,tmp;
+	char command,tmp;
 	char name[30];
 	bzero(name,30);
 	while(check){
-		printf("(C)reate the filesystem\n");
-		printf("(D)estroy the filesystem\n");
-		printf("(Q)uit\n");
-		scanf("%c%c",&ch,&tmp);
-		if(ch == 'C'){
+		printf("(C)reate the filesystem.\n");
+		printf("(D)estroy the filesystem.\n");
+		printf("(Q)uit.\n");
+		scanf("%c%c",&command,&tmp);
+		if(command == 'C'){
 			printf("Enter the system name: ");
 			scanf("%s%c",name,&tmp);
-			printf("Enter the system size(kB):");
+			printf("Enter the system size in KB :");
 			int value;
 			scanf("%d%c",&value,&tmp);
 			myfs_create(name,value);
 		}
-		else if(ch == 'D'){
+		else if(command == 'D'){
 			printf("Enter the system name: ");
 			scanf("%s%c",name,&tmp);
 			myfs_destroy(name);
 		}
-		else if(ch == 'Q')		
+		else if(command == 'Q')		
 			check = 0;
 		else
-			printf("some wrong input\n");
+			printf("No this command.\n");
 	}
 }
